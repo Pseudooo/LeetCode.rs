86d6ea2c46a6e5addced2e7af464221743c9beaf
Mitchell Wright <mitchellw@digitalwonderlab.com>
2024-03-11 20:15:31 +0000

    Debugging

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use crate::reverse_words::reverse_words;
+use crate::two_sum::two_sum;
+
 mod reverse_words;
 mod two_sum;
 
 fn main() {
-    println!("{}", reverse_words("the sky is blue".to_string()));
-    println!("{}", reverse_words("  hello world  ".to_string()));
-    println!("{}", reverse_words("a good   example".to_string()));
+    let numbers = [3,2,4].iter().cloned().collect();
+    let target = 6;
+    let expected: Vec<i32> = [1,2].iter().cloned().collect();
+
+    let result = two_sum(numbers, target);
+    assert_eq!(result, expected);
 }
